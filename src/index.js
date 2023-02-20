75d98e77231125e2e5b22a6067f355e8b406fe0e
Danila <danila.ban@mail.ru>
2023-02-20 16:26:43 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) return false;
+  let resultBool = true;
+  let dontInclude;
+  while (str.length > 0 && resultBool) {
+    dontInclude = [];
+    bracketsConfig.forEach((elem) => {
+      const a = elem.join('');
+      if (str.includes(a)) {
+        str = str.slice(0, str.indexOf(a)) + str.slice(str.indexOf(a) + 2);
+        dontInclude.push('false')
+      } else {
+        dontInclude.push('true')
+      };
+    })
+    let count = 0;
+    for (let val of dontInclude) {
+        if (val === 'true') count++
+    }
+     resultBool = (count === dontInclude.length) ? false : true;
+  }
+  return resultBool;
 }
